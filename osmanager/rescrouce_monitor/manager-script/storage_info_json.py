b6b01697e64f7d0c34169fb006a0e968af909b55
liuk311 <liuk311@chinaunicom.cn>
2026-05-05 20:28:38 +0800

    Implement storage_info_json get hardware info

---
+++ osmanager/rescrouce_monitor/manager-script/storage_info_json.py
@@ -0,0 +1,27 @@
+import json
+import subprocess
+import sys
+
+def get_hardware_info(device):
+    """获取磁盘硬件信息"""
+    try:
+        udev_cmd = f'udevadm info --query=property --name=/dev/{device}'
+        udev_info = subprocess.check_output(udev_cmd, shell=True, text=True)
+        info = {'ID_VENDOR': '', 'ID_MODEL': '', 'ID_REVISION': '', 'ID_SERIAL_SHORT': ''}
+        for line in udev_info.splitlines():
+            if '=' in line:
+                key, value = line.split('=', 1)
+                if key in info:
+                    info[key] = value
+        return info
+    except Exception:
+        return {}
+
+def process_device(dev):
+    """递归处理设备及其子设备"""
+    pass
+
+def main():
+    pass
+if __name__ == '__main__':
+    main()
