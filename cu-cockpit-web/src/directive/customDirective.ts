13ea007818691e36bd816c38d43a019445ff134c
liuk311 <liuk311@chinaunicom.cn>
2026-05-22 13:54:26 +0800

    Implement customDirective

---
+++ cu-cockpit-web/src/directive/customDirective.ts
@@ -0,0 +1,7 @@
+import type { App } from 'vue';
+
+/**
+ * 按钮波浪指令
+ * @directive 默认方式：v-waves，如 `<div v-waves></div>`
+ * @directive 参数方式：v-waves=" |light|red|orange|purple|green|teal"，如 `<div v-waves="'light'"></div>`
+ */
