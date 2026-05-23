55764b99cce8bc952d4474763d6fb5bee09a0f05
liuk311 <liuk311@chinaunicom.cn>
2026-05-23 18:08:21 +0800

    Implement views.d

---
+++ cu-cockpit-web/src/types/views.d.ts
@@ -0,0 +1,15 @@
+/**
+ * views personal
+ */
+type NewInfo = {
+	title: string;
+	date: string;
+	link: string;
+};
+type Recommend = {
+	title: string;
+	msg: string;
+	icon: string;
+	bg: string;
+	iconColor: string;
+};
