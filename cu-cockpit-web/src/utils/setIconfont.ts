493e50bc016948d862389945956185145f04dd6e
liuk311 <liuk311@chinaunicom.cn>
2026-05-23 23:46:01 +0800

    Scaffold setIconfont function with signatures and docstrings.

---
+++ cu-cockpit-web/src/utils/setIconfont.ts
@@ -0,0 +1,10 @@
+// 字体图标 url
+const cssCdnUrlList: Array<string> = [
+	// '//at.alicdn.com/t/font_2298093_y6u00apwst.css',
+	// '//at.alicdn.com/t/c/font_3882322_9ah7y8m9175.css', //项目用icon
+	//'//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+];
+// 第三方 js url
+const jsCdnUrlList: Array<string> = [];
+
+// 动态批量设置字体图标
