6f70d5c0c39282ba7d3713a0231b0bc070e402da
liuk311 <liuk311@chinaunicom.cn>
2026-05-23 10:23:08 +0800

    Draft frontEnd control route signatures and skeletons

---
+++ cu-cockpit-web/src/router/frontEnd.ts
@@ -0,0 +1,34 @@
+import { RouteRecordRaw } from 'vue-router';
+import { storeToRefs } from 'pinia';
+import { formatTwoStageRoutes, formatFlatteningRoutes, router } from '/@/router/index';
+import { dynamicRoutes, notFoundAndNoPower } from '/@/router/route';
+import pinia from '/@/stores/index';
+import { Session } from '/@/utils/storage';
+import { useUserInfo } from '/@/stores/userInfo';
+import { useTagsViewRoutes } from '/@/stores/tagsViewRoutes';
+import { useRoutesList } from '/@/stores/routesList';
+import { NextLoading } from '/@/utils/loading';
+
+// 前端控制路由
+
+/**
+ * 前端控制路由：初始化方法，防止刷新时路由丢失
+ * @method  NextLoading 界面 loading 动画开始执行
+ * @method useUserInfo(pinia).setUserInfos() 触发初始化用户信息 pinia
+ * @method setAddRoute 添加动态路由
+ * @method setFilterMenuAndCacheTagsViewRoutes 设置递归过滤有权限的路由到 pinia routesList 中（已处理成多级嵌套路由）及缓存多级嵌套数组处理后的一维数组
+ */
+export async function initFrontEndControlRoutes() {
+	// 界面 loading 动画开始执行
+	if (window.nextLoading === undefined) NextLoading.start();
+	// 无 token 停止执行下一步
+	// if (!Session.get('token')) return false;
+	// // 触发初始化用户信息 pinia
+	// await useUserInfo(pinia).setUserInfos();
+	// // 无登录权限时，添加判断
+	// if (useUserInfo().userInfos.roles.length <= 0) return Promise.resolve(true);
+	// 添加动态路由
+	// await setAddRoute();
+	// 设置递归过滤有权限的路由到 pinia routesList 中（已处理成多级嵌套路由）及缓存多级嵌套数组处理后的一维数组
+	await setFilterMenuAndCacheTagsViewRoutes();
+}
