3b80f8631393d927555c6e636d1ec4946efd9445
liuk311 <liuk311@chinaunicom.cn>
2026-05-23 20:45:15 +0800

    Implement formatTime

---
+++ cu-cockpit-web/src/utils/formatTime.ts
@@ -0,0 +1,11 @@
+/**
+ * 时间日期转换
+ * @param date 当前时间，new Date() 格式
+ * @param format 需要转换的时间格式字符串
+ * @description format 字符串随意，如 `YYYY-mm、YYYY-mm-dd`
+ * @description format 季度："YYYY-mm-dd HH:MM:SS QQQQ"
+ * @description format 星期："YYYY-mm-dd HH:MM:SS WWW"
+ * @description format 几周："YYYY-mm-dd HH:MM:SS ZZZ"
+ * @description format 季度 + 星期 + 几周："YYYY-mm-dd HH:MM:SS WWW QQQQ ZZZ"
+ * @returns 返回拼接后的时间字符串
+ */
