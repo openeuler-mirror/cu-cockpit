a9ad053a17c1ead8c5947c52f3de765c64e283c3
liuk311 <liuk311@chinaunicom.cn>
2026-05-23 17:21:40 +0800

    Scaffold pinia.d with type signatures and docstrings

---
+++ cu-cockpit-web/src/types/pinia.d.ts
@@ -0,0 +1,23 @@
+/**
+ * pinia 类型定义
+ */
+
+// 用户信息
+declare interface UserInfosState<T = any> {
+	userInfos: {
+		authBtnList: string[];
+		photo: string;
+		roles: string[];
+		time: number;
+		username: string;
+		[key: string]: T;
+	};
+}
+
+// 路由缓存列表
+declare interface KeepAliveNamesState {
+	keepAliveNames: string[];
+	cachedViews: string[];
+}
+
+// 后端返回原始路由(未处理时)
