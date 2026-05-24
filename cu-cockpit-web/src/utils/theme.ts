22ba5937855ec1df226aff732d6663b2006f051f
liuk311 <liuk311@chinaunicom.cn>
2026-05-24 20:23:39 +0800

    Scaffold theme module with signatures and docstrings

---
+++ cu-cockpit-web/src/utils/theme.ts
@@ -0,0 +1,9 @@
+import { ElMessage } from 'element-plus';
+
+/**
+ * 颜色转换函数
+ * @method hexToRgb hex 颜色转 rgb 颜色
+ * @method rgbToHex rgb 颜色转 Hex 颜色
+ * @method getDarkColor 加深颜色值
+ * @method getLightColor 变浅颜色值
+ */
