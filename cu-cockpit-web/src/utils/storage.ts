9fc6521cd1fcf041d0aa6655504db148913a401f
liuk311 <liuk311@chinaunicom.cn>
2026-05-25 18:57:24 +0800

    Flesh out storage utilities with implementation logic

--- cu-cockpit-web/src/utils/storage.ts
+++ cu-cockpit-web/src/utils/storage.ts
@@ -7,3 +7,30 @@ import Cookies from 'js-cookie';
  * @method remove 移除永久缓存
  * @method clear 移除全部永久缓存
  */
+export const Local = {
+	// 设置永久缓存
+	set(key: string, val: any) {
+		window.localStorage.setItem(key, JSON.stringify(val));
+	},
+	// 获取永久缓存
+	get(key: string) {
+		let json = <string>window.localStorage.getItem(key);
+		return JSON.parse(json);
+	},
+	// 移除永久缓存
+	remove(key: string) {
+		window.localStorage.removeItem(key);
+	},
+	// 移除全部永久缓存
+	clear() {
+		window.localStorage.clear();
+	},
+};
+
+/**
+ * window.sessionStorage 浏览器临时缓存
+ * @method set 设置临时缓存
+ * @method get 获取临时缓存
+ * @method remove 移除临时缓存
+ * @method clear 移除全部临时缓存
+ */
