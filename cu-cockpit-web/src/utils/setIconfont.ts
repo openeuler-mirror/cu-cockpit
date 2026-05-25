2d4967880c236633dda2cc9f213fb714c60bdf68
liuk311 <liuk311@chinaunicom.cn>
2026-05-25 18:45:32 +0800

    Wire up iconfont script injection inside setIconfont.

--- cu-cockpit-web/src/utils/setIconfont.ts
+++ cu-cockpit-web/src/utils/setIconfont.ts
@@ -8,3 +8,23 @@ const cssCdnUrlList: Array<string> = [
 const jsCdnUrlList: Array<string> = [];
 
 // 动态批量设置字体图标
+export function setCssCdn() {
+	if (cssCdnUrlList.length <= 0) return false;
+	cssCdnUrlList.map((v) => {
+		let link = document.createElement('link');
+		link.rel = 'stylesheet';
+		link.href = v;
+		link.crossOrigin = 'anonymous';
+		document.getElementsByTagName('head')[0].appendChild(link);
+	});
+}
+
+// 动态批量设置第三方js
+export function setJsCdn() {
+	if (jsCdnUrlList.length <= 0) return false;
+	jsCdnUrlList.map((v) => {
+		let link = document.createElement('script');
+		link.src = v;
+		document.body.appendChild(link);
+	});
+}
