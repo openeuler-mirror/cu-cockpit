db41b461aeb1253aa152fde9969a5242c6a9e162
liuk311 <liuk311@chinaunicom.cn>
2026-05-25 12:08:10 +0800

    Add core state and getter logic to frontendMenu

--- cu-cockpit-web/src/stores/frontendMenu.ts
+++ cu-cockpit-web/src/stores/frontendMenu.ts
@@ -25,3 +25,23 @@ const dynamicViewsModules: Record<string, Function> = Object.assign({}, { ...lay
  * @param component 当前要处理项 component
  * @returns 返回处理成函数后的 component
  */
+export function dynamicImport(dynamicViewsModules: Record<string, Function>, component: string) {
+    const keys = Object.keys(dynamicViewsModules);
+    const matchKeys = keys.filter((key) => {
+        const k = key.replace(/..\/views|../, '');
+        return k.startsWith(`${component}`) || k.startsWith(`/${component}`);
+    });
+    if (matchKeys?.length === 1) {
+        const matchKey = matchKeys[0];
+        return dynamicViewsModules[matchKey];
+    }
+    if (matchKeys?.length > 1) {
+        return false;
+    }
+}
+
+/**
+ * @description: 处理后端菜单数据格式
+ * @param {Array} menuData
+ * @return {*}
+ */
