ccdcd4a1a594c98256e30a4965ec74820346671c
liuk311 <liuk311@chinaunicom.cn>
2026-05-25 16:51:52 +0800

    Add core business logic implementations

--- cu-cockpit-web/src/utils/cores.tsx
+++ cu-cockpit-web/src/utils/cores.tsx
@@ -4,3 +4,16 @@ export interface TaskProps {
 	name: string;
 	custom?: any;
 }
+
+// 定义自定义事件类型
+export type BusEvents = {
+	onNewTask: TaskProps | undefined;
+};
+
+export interface Task {
+	id: number;
+	handle: string;
+	data: any;
+	createTime: Date;
+	custom?: any;
+}
