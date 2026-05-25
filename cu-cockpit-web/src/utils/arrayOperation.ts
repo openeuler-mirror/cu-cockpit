81a60ae79155aefe5b65d344d45709505bda880d
liuk311 <liuk311@chinaunicom.cn>
2026-05-25 15:35:29 +0800

    Add deep object comparison logic to array utilities

--- cu-cockpit-web/src/utils/arrayOperation.ts
+++ cu-cockpit-web/src/utils/arrayOperation.ts
@@ -16,3 +16,28 @@ export function judementSameArr(newArr: unknown[] | string[], oldArr: string[]):
 	}
 	return count === leng ? true : false;
 }
+
+/**
+ * 判断两个对象是否相同
+ * @param a 要比较的对象一
+ * @param b 要比较的对象二
+ * @returns 相同返回 true，反之则反
+ */
+export function isObjectValueEqual<T>(a: T, b: T): boolean {
+	if (!a || !b) return false;
+	let aProps = Object.getOwnPropertyNames(a);
+	let bProps = Object.getOwnPropertyNames(b);
+	if (aProps.length != bProps.length) return false;
+	for (let i = 0; i < aProps.length; i++) {
+		let propName = aProps[i];
+		let propA = a[propName];
+		let propB = b[propName];
+		if (!b.hasOwnProperty(propName)) return false;
+		if (propA instanceof Object) {
+			if (!isObjectValueEqual(propA, propB)) return false;
+		} else if (propA !== propB) {
+			return false;
+		}
+	}
+	return true;
+}
