1ef6604af0e0ea08ad4c60fcf323545fe60d048c
liuk311 <liuk311@chinaunicom.cn>
2026-05-27 16:06:18 +0800

    Flesh out request handlers, interceptors and error handling

--- cu-cockpit-web/src/utils/request.ts
+++ cu-cockpit-web/src/utils/request.ts
@@ -31,3 +31,54 @@ service.interceptors.request.use(
 );
 
 // 添加响应拦截器
+service.interceptors.response.use(
+	(response) => {
+		// 对响应数据做点什么
+		const res = response.data;
+		if (res.code && res.code !== 0) {
+				console.log(res);
+			// `token` 过期或者账号已在别处登录
+			if (res.code === 401 || res.code === 4001) {
+				Session.clear(); // 清除浏览器全部临时缓存
+				window.location.href = '/'; // 去登录页
+				ElMessageBox.alert('无权限访问或会话过期，请重新登录', '提示', {
+          customClass: 'single-alert',
+        })
+					.then(() => {})
+					.catch(() => {});
+			}
+			// return Promise.reject(service.interceptors.response);
+			return response.data;
+		} else {
+			return response.data;
+		}
+	},
+	(error) => {
+		// 对响应错误做点什么
+		if (error.message.indexOf('timeout') != -1) {
+			ElMessage.error('网络超时');
+		} else if (error.message == 'Network Error') {
+			ElMessage.error('网络连接错误');
+		} else if (error.status == '500') {
+			ElMessage.error('服务器内部错误');
+		} else {
+			if (error.response.data) {
+        if (error.response.data.code == 401) {
+          ElMessageBox.alert('无权限访问或会话过期，请重新登录', '提示', {
+            customClass: 'single-alert',
+            confirmButtonText: '确定',
+            callback: () => {
+              Session.clear(); // 清除浏览器全部临时缓存
+              window.location.href = '/'; // 去登录页
+            }
+          })
+        } else {
+          ElMessage.error(error.response.statusText);
+        }
+      } else {
+        ElMessage.error('接口路径找不到');
+      }
+		}
+		return Promise.reject(error);
+	}
+);
