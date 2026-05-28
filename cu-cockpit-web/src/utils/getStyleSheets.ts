02db7e0333098bab2abe0b96e80ede3ef862ed07
liuk311 <liuk311@chinaunicom.cn>
2026-05-28 16:00:07 +0800

    Seal up remaining implementation details

--- cu-cockpit-web/src/utils/getStyleSheets.ts
+++ cu-cockpit-web/src/utils/getStyleSheets.ts
@@ -86,3 +86,27 @@ const getAwesomeIconfont = () => {
 		});
 	});
 };
+
+/**
+ * 获取字体图标 `document.styleSheets`
+ * @method ali 获取阿里字体图标 `<i class="iconfont 图标类名"></i>`
+ * @method ele 获取 element plus 自带图标 `<i class="图标类名"></i>`
+ * @method ali 获取 fontawesome 的图标 `<i class="fa 图标类名"></i>`
+ */
+const initIconfont = {
+	// iconfont
+	ali: () => {
+		return getAlicdnIconfont();
+	},
+	// element plus
+	ele: () => {
+		return getElementPlusIconfont();
+	},
+	// fontawesome
+	awe: () => {
+		return getAwesomeIconfont();
+	},
+};
+
+// 导出方法
+export default initIconfont;
