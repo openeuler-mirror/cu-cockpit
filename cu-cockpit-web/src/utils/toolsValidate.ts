bcf3320652be1e9e1f39a4319985c2b270f9745c
liuk311 <liuk311@chinaunicom.cn>
2026-05-28 14:08:32 +0800

    Implement toolsValidate verifyCnAndSpace

--- cu-cockpit-web/src/utils/toolsValidate.ts
+++ cu-cockpit-web/src/utils/toolsValidate.ts
@@ -78,3 +78,17 @@ export function verifiyNumberInteger(val: string) {
 	// 返回结果
 	return v;
 }
+
+/**
+ * 去掉中文及空格
+ * @param val 当前值字符串
+ * @returns 返回处理后的字符串
+ */
+export function verifyCnAndSpace(val: string) {
+	// 匹配中文与空格
+	let v = val.replace(/[\u4e00-\u9fa5\s]+/g, '');
+	// 匹配空格
+	v = v.replace(/(^\s*)|(\s*$)/g, '');
+	// 返回结果
+	return v;
+}
