87c85c450a57c4a1e4985541d1b2e52ad9fead3c
liuk311 <liuk311@chinaunicom.cn>
2026-05-28 14:44:43 +0800

    Hook up lazy-loaded components and route metadata

--- cu-cockpit-web/src/router/index.ts
+++ cu-cockpit-web/src/router/index.ts
@@ -109,3 +109,53 @@ const checkToken = ()=>{
     }
 }
 // 路由加载前
+router.beforeEach(async (to, from, next) => {
+    //2025-0904
+    // 检查浏览器本地版本与线上版本是否一致，判断是否需要刷新页面进行更新
+    await checkVersion()
+    checkToken()
+    NProgress.configure({showSpinner: false});
+    if (to.meta.title) NProgress.start();
+    const token = Session.get('token');
+    if (to.path === '/login' && !token) {
+        next();
+        NProgress.done();
+    } else {
+        if (!token) {
+            next(`/login?redirect=${to.path}&params=${JSON.stringify(to.query ? to.query : to.params)}`);
+            Session.clear();
+            NProgress.done();
+        }
+        // else if (token && to.path === '/login' && userInfos.value.pwd_change_count===0 ) {
+        //     next('/login');
+        //     NProgress.done();
+        // } else if (token && to.path === '/login' && userInfos.value.pwd_change_count>0) {
+        //     next('/home');
+        //     NProgress.done();
+        // }else if(token &&  frameOutRoutes.includes(to.path) ){
+        //     next()
+        // }
+         else if (token && to.path === '/login') {
+            next('/overview');
+            NProgress.done();
+        }else {
+            const storesRoutesList = useRoutesList(pinia);
+            const {routesList} = storeToRefs(storesRoutesList);
+            if (routesList.value.length === 0) {
+                // if (isRequestRoutes) {
+                //     // 后端控制路由：路由数据初始化，防止刷新时丢失
+                //     await initBackEndControlRoutes();
+                //     // 解决刷新时，一直跳 404 页面问题，关联问题 No match found for location with path 'xxx'
+                //     // to.query 防止页面刷新时，普通路由带参数时，参数丢失。动态路由（xxx/:id/:name"）isDynamic 无需处理
+
+                //     next({ path: to.path, query: to.query });
+                // } else {
+                    await initFrontEndControlRoutes();
+                    next({ path: to.path, query: to.query });
+                // }
+            } else {
+                next();
+            }
+        }
+    }
+});
