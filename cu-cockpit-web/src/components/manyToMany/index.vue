59a29c962be0e069a92b8e8a3b104fee39f2682b
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 12:13:14 +0800

    Scaffold index template structure

---
+++ cu-cockpit-web/src/components/manyToMany/index.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <!--   你的自定义受控组件-->
+  <div>
+    <el-tag class="many-to-many-tag" :type="randomType" v-for="(item,index) in data" :key="index">{{item}}</el-tag>
+  </div>
+</template>
+<script lang="ts" setup>
+import {watch, ref} from "vue";
+</script>
+<style scoped>
+</style>
