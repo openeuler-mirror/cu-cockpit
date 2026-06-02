ab17d986c7ceb0ec143d498592f5f7ceebb5788c
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 18:09:12 +0800

    Scaffold defaults component with empty signatures and docstrings.

---
+++ cu-cockpit-web/src/layout/main/defaults.vue
@@ -0,0 +1,19 @@
+<template>
+
+	<el-container class="layout-container">
+		<LayoutAside />
+		<el-container class="layout-container-view h100">
+			<el-scrollbar ref="layoutScrollbarRef" class="layout-backtop">
+				<LayoutHeader />
+				<LayoutMain ref="layoutMainRef" />
+			</el-scrollbar>
+		</el-container>
+	</el-container>
+</template>
+<script setup lang="ts" name="layoutDefaults">
+import { defineAsyncComponent, watch, onMounted, nextTick, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { storeToRefs } from 'pinia';
+import { useThemeConfig } from '/@/stores/themeConfig';
+import { NextLoading } from '/@/utils/loading';
+</script>
