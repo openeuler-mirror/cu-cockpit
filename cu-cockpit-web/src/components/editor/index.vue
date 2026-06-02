9baa6ac4fd70c642691fbad155140545be6cbd2d
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 10:51:59 +0800

    Scaffold index component with method signatures and docstring placeholde

---
+++ cu-cockpit-web/src/components/editor/index.vue
@@ -0,0 +1,20 @@
+<template>
+
+	<div class="editor-container">
+		<Toolbar :editor="editorRef" :mode="mode" />
+		<Editor
+			:mode="mode"
+			:defaultConfig="state.editorConfig"
+			:style="{ height }"
+			v-model="state.editorVal"
+			@onCreated="handleCreated"
+			@onChange="handleChange"
+		/>
+	</div>
+</template>
+<script setup lang="ts" name="wngEditor">
+import '@wangeditor/editor/dist/css/style.css';
+import { reactive, shallowRef, watch, onBeforeUnmount } from 'vue';
+import { IDomEditor } from '@wangeditor/editor';
+import { Toolbar, Editor } from '@wangeditor/editor-for-vue';
+</script>
