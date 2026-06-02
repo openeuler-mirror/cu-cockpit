27922afa99c27701ea2b38cdccdf0a56a6dfe2dc
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 15:49:42 +0800

    Scaffold classic template skeleton with signatures and docstrings

---
+++ cu-cockpit-web/src/layout/main/classic.vue
@@ -0,0 +1,19 @@
+<template>
+
+	<el-container class="layout-container flex-center">
+		<LayoutHeader />
+		<el-container class="layout-mian-height-50">
+			<LayoutAside />
+			<div class="flex-center layout-backtop">
+				<LayoutTagsView v-if="isTagsview" />
+				<LayoutMain ref="layoutMainRef" />
+			</div>
+		</el-container>
+	</el-container>
+</template>
+<script setup lang="ts" name="layoutClassic">
+import { defineAsyncComponent, computed, ref, watch, nextTick, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { storeToRefs } from 'pinia';
+import { useThemeConfig } from '/@/stores/themeConfig';
+</script>
