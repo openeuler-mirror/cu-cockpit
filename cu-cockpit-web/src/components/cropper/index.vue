8d6cd19223e07043a35d096e32e33eca412a0e09
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 10:43:52 +0800

    Scaffold cropper component skeleton with docstrings

---
+++ cu-cockpit-web/src/components/cropper/index.vue
@@ -0,0 +1,10 @@
+<template>
+
+</template>
+<script setup lang="ts" name="cropper">
+import { reactive, nextTick } from 'vue';
+import Cropper from 'cropperjs';
+import 'cropperjs/dist/cropper.css';
+</script>
+<style scoped lang="scss">
+</style>
