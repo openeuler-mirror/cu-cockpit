3e113d8d01ff3018e9146b7f5bd9e132dee78fd8
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 15:15:53 +0800

    Scaffold search component skeleton with docstrings.

---
+++ cu-cockpit-web/src/layout/navBars/breadcrumb/search.vue
@@ -0,0 +1,17 @@
+<template #default="{ item }">
+
+						<div>
+							<SvgIcon :name="item.meta.icon" class="mr5" />
+							{{ $t(item.meta.title) }}
+						</div>
+					
+</template>
+<script setup lang="ts" name="layoutBreadcrumbSearch">
+import { reactive, ref, nextTick } from 'vue';
+import { useRouter } from 'vue-router';
+import { useI18n } from 'vue-i18n';
+import { storeToRefs } from 'pinia';
+import { useTagsViewRoutes } from '/@/stores/tagsViewRoutes';
+</script>
+<style scoped lang="scss">
+</style>
