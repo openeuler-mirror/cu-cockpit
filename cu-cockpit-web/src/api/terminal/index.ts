ef8056a9f87866d3094890d80256172046306481
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 09:52:51 +0800

    Draft index connectType signatures with documentation

---
+++ cu-cockpit-web/src/api/terminal/index.ts
@@ -0,0 +1,10 @@
+import request from '/@/utils/request';
+import Cookies from 'js-cookie'
+
+type connectType = {
+  hostname: string,
+  port: number,
+  username: string,
+  password: string,
+  term: string
+}
