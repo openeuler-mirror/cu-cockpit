c787dd9c53be13fae7f3deb780e85615823aae5e
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 14:18:08 +0800

    Add settings module skeleton with type signatures

---
+++ cu-cockpit-web/src/settings.ts
@@ -0,0 +1,15 @@
+// 引入fast-crud
+// import {FastCrud, useTypes} from '@fast-crud/fast-crud';
+
+// const {getType} = useTypes();
+// import '@fast-crud/fast-crud/dist/style.css';
+// import {setLogger} from '@fast-crud/fast-crud';
+import {getBaseURL} from '/@/utils/baseUrl';
+// element
+import ui from '@fast-crud/ui-element';
+import {request} from '/@/utils/service';
+//扩展包
+// import {FsExtendsEditor, FsExtendsUploader } from '@fast-crud/fast-extends';
+// import '@fast-crud/fast-extends/dist/style.css';
+import {successNotification} from '/@/utils/message';
+import XEUtils from "xe-utils";
