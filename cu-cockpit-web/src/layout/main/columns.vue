a71a795722b6332dcb42aa991bdcfe3cc658eeb9
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 16:05:12 +0800

    Build initial columns component skeleton

---
+++ cu-cockpit-web/src/layout/main/columns.vue
@@ -0,0 +1,19 @@
+<template>
+
+	<el-container class="layout-container">
+		<ColumnsAside />
+		<el-container class="layout-columns-warp layout-container-view h100">
+			<LayoutAside />
+			<el-scrollbar ref="layoutScrollbarRef" class="layout-backtop">
+				<LayoutHeader />
+				<LayoutMain ref="layoutMainRef" />
+			</el-scrollbar>
+		</el-container>
+	</el-container>
+</template>
+<script setup lang="ts" name="layoutColumns">
+import { defineAsyncComponent, watch, onMounted, nextTick, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { storeToRefs } from 'pinia';
+import { useThemeConfig } from '/@/stores/themeConfig';
+</script>
