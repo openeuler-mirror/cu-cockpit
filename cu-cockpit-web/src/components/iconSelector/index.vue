6feda5bce39d6646fbd682bca567f013f0bbff61
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 17:44:06 +0800

    Sketch out icon selector component skeleton and template

---
+++ cu-cockpit-web/src/components/iconSelector/index.vue
@@ -0,0 +1,9 @@
+<template #default>
+
+</template>
+<script setup lang="ts" name="iconSelector">
+import { defineAsyncComponent, ref, reactive, onMounted, nextTick, computed, watch } from 'vue';
+import type { TabsPaneContext } from 'element-plus';
+import initIconfont from '/@/utils/getStyleSheets';
+import '/@/theme/iconSelector.scss';
+</script>
