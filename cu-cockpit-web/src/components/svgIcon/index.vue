86a554e41fb763c4c64e66f1b174baa93bd960fc
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 12:43:00 +0800

    Scaffold index component template skeleton

---
+++ cu-cockpit-web/src/components/svgIcon/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+	<i v-if="isShowIconSvg" class="el-icon" :style="setIconSvgStyle">
+		<component :is="getIconName" />
+	</i>
+	<div v-else-if="isShowIconImg" :style="setIconImgOutStyle">
+		<img :src="getIconName" :style="setIconSvgInsStyle" />
+	</div>
+	<i v-else :class="getIconName" :style="setIconSvgStyle" />
+</template>
+<script setup lang="ts" name="svgIcon">
+import { computed } from 'vue';
+</script>
