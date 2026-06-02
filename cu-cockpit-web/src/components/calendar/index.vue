4b0323bac051f10572744b1beb18978bae0127b5
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 10:21:32 +0800

    Scaffold index component skeleton with type signatures

---
+++ cu-cockpit-web/src/components/calendar/index.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+<script lang="ts" setup>
+  import { useUi } from '@fast-crud/fast-crud';
+  import { ref, defineProps, PropType, watch, computed, onMounted } from 'vue';
+  import Holidays from 'date-holidays';
+  import Lunar from 'lunar-javascript';
+</script>
+<style lang="scss" scoped>
+</style>
