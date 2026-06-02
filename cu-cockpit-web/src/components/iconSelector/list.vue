1024e2e9dcb720468dde4c330c60a5abb702ac84
liuk311 <liuk311@chinaunicom.cn>
2026-06-02 11:45:30 +0800

    Sketch out list component template

---
+++ cu-cockpit-web/src/components/iconSelector/list.vue
@@ -0,0 +1,19 @@
+<template>
+
+	<div class="icon-selector-warp-row">
+		<el-scrollbar ref="selectorScrollbarRef">
+			<el-row :gutter="10" v-if="props.list.length > 0">
+				<el-col :xs="6" :sm="4" :md="4" :lg="4" :xl="4" v-for="(v, k) in list" :key="k" @click="onColClick(v)">
+					<div class="icon-selector-warp-item" :class="{ 'icon-selector-active': prefix === v }">
+						<SvgIcon :name="v" />
+					</div>
+				</el-col>
+			</el-row>
+			<el-empty :image-size="100" v-if="list.length <= 0" :description="empty"></el-empty>
+		</el-scrollbar>
+	</div>
+</template>
+<script setup lang="ts" name="iconSelectorList">
+</script>
+<style scoped lang="scss">
+</style>
