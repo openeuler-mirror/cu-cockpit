57a63b54d04dd243e00100080cd35983645e6b10
liuk311 <liuk311@chinaunicom.cn>
2026-06-03 20:49:25 +0800

    Draft tableSelector component skeleton

---
+++ cu-cockpit-web/src/components/tableSelector/index.vue
@@ -0,0 +1,10 @@
+<template>
+
+</template>
+<script setup lang="ts">
+import { computed, defineProps, onMounted, reactive, ref, watch } from 'vue';
+import XEUtils from 'xe-utils';
+import { request } from '/@/utils/service';
+</script>
+<style lang="scss">
+</style>
