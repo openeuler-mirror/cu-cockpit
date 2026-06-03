97973b17c8b2a5a41ce7323a750b78322e52f5a3
liuk311 <liuk311@chinaunicom.cn>
2026-06-03 09:48:40 +0800

    Scaffold aside component skeleton with signatures and docstrings

---
+++ cu-cockpit-web/src/layout/component/aside.vue
@@ -0,0 +1,21 @@
+<template>
+
+	<div class="h100" v-show="!isTagsViewCurrenFull">
+		<el-aside class="layout-aside" :class="setCollapseStyle">
+			<Logo v-if="setShowLogo" />
+			<el-scrollbar class="flex-auto" ref="layoutAsideScrollbarRef" @mouseenter="onAsideEnterLeave(true)" @mouseleave="onAsideEnterLeave(false)">
+				<Vertical :menuList="state.menuList" />
+			</el-scrollbar>
+		</el-aside>
+	</div>
+</template>
+<script setup lang="ts" name="layoutAside">
+import { defineAsyncComponent, reactive, computed, watch, onBeforeMount, ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import pinia from '/@/stores/index';
+import { useRoutesList } from '/@/stores/routesList';
+import { useThemeConfig } from '/@/stores/themeConfig';
+import { useTagsViewRoutes } from '/@/stores/tagsViewRoutes';
+import mittBus from '/@/utils/mitt';
+import { useRoute } from 'vue-router';
+</script>
