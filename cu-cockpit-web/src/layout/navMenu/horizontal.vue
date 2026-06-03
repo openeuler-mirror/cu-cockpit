8c9114d477a127edcd8dad9c5e8c8a3f3fd55228
liuk311 <liuk311@chinaunicom.cn>
2026-06-03 17:49:13 +0800

    Scaffold horizontal navigation template structure

---
+++ cu-cockpit-web/src/layout/navMenu/horizontal.vue
@@ -0,0 +1,19 @@
+<template #title v-else>
+
+								<a class="w100" @click.prevent="onALinkClick(val)">
+									<SvgIcon :name="val.meta.icon" />
+									{{ $t(val.meta.title) }}
+								</a>
+							
+</template>
+<script setup lang="ts" name="navMenuHorizontal">
+import { defineAsyncComponent, reactive, computed, onMounted, nextTick, onBeforeMount, ref } from 'vue';
+import {useRoute, onBeforeRouteUpdate, RouteRecordRaw, useRouter} from 'vue-router';
+import { storeToRefs } from 'pinia';
+import { useRoutesList } from '/@/stores/routesList';
+import { useThemeConfig } from '/@/stores/themeConfig';
+import other from '/@/utils/other';
+import mittBus from '/@/utils/mitt';
+</script>
+<style scoped lang="scss">
+</style>
