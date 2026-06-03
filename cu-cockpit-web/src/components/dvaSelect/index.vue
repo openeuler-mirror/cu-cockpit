51f7506ee019c7aea18de62afbc9e1bcf37a2841
liuk311 <liuk311@chinaunicom.cn>
2026-06-03 20:35:04 +0800

    Scaffold index template with placeholder markup (index)

---
+++ cu-cockpit-web/src/components/dvaSelect/index.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <!--   你的自定义受控组件-->
+  <el-select-v2
+      v-model="data"
+      :options="options"
+      style="width: 100%;"
+      :clearable="true"
+      :props="selectProps"
+      @change="onDataChange"
+
+  />
+</template>
+<script lang="ts" setup>
+import {ref, defineComponent, watch, computed, toRefs, toRaw, onMounted} from 'vue'
+import {useUi} from "@fast-crud/fast-crud";
+import {request} from "/@/utils/service";
+</script>
+<style scoped lang="scss">
+</style>
