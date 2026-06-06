84a5629490ba8d7baf2faa3a8126db73d20dc812
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-06 22:03:32 +0800

    Build index template (index 4)

---
+++ cu-cockpit-web/src/views/system/menu/components/MenuButtonCom/index.vue
@@ -0,0 +1,15 @@
+<template #default="scope">
+
+								<el-button text type="info" :icon="Close" @click="removeSelectedRows(scope.row)" circle />
+							
+</template>
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+import { useFs } from '@fast-crud/fast-crud';
+import { createCrudOptions } from './crud';
+import { MenuTreeItemType } from '../../types';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import XEUtils from 'xe-utils';
+import { BatchDelete } from './api';
+import { Close, Delete } from '@element-plus/icons-vue';
+</script>
