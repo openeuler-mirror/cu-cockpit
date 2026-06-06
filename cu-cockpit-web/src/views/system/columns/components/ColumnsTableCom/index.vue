dbf3530843673f6f49c46ecdfa94e0f0ec727f6e
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-06 14:47:10 +0800

    Scaffold index component with signatures and docstrings

---
+++ cu-cockpit-web/src/views/system/columns/components/ColumnsTableCom/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+<script lang="ts" setup>
+import { ref, reactive } from 'vue';
+import { ElMessageBox } from 'element-plus';
+import ColumnsFormCom from '../ColumnsFormCom/index.vue';
+import { getColumnsData, automatchColumnsData, deleteColumnsData, updateColumnsData } from './api';
+import { successNotification, warningNotification } from '/@/utils/message';
+import { APIResponseData, CurrentInfoType, ColumnsFormDataType, AddColumnsDataType } from '../../types';
+</script>
+<style lang="scss" scoped>
+</style>
