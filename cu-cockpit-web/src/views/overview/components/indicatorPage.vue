bd88344e5a3e4769a06cd174c339ea3b68823a02
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-06 13:34:41 +0800

    Set up indicatorPage HTML skeleton

---
+++ cu-cockpit-web/src/views/overview/components/indicatorPage.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+<script lang="ts" setup name="indicator">
+import { reactive, onMounted, ref, onUnmounted } from 'vue';
+import * as echarts from 'echarts';
+import { monitorStatus, hardInfo } from '/@/api/run/run';
+import { useRouter } from 'vue-router';
+</script>
+<style scoped lang="scss">
+</style>
