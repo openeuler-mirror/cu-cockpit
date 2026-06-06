889b0841ed57f6a8f0bfa9739742605c3c2e10bc
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-06 16:47:00 +0800

    Scaffold dept tree component with basic file structure and signatures

---
+++ cu-cockpit-web/src/views/system/dept/components/DeptTreeCom/index.vue
@@ -0,0 +1,16 @@
+<template>
+
+	<el-input v-model="filterVal" :prefix-icon="Search" placeholder="请输入部门名称" />
+</template>
+<script lang="ts" setup>
+import { ref, watch, toRaw, h } from 'vue';
+import { ElTree } from 'element-plus';
+import { getElementLabelLine } from 'element-tree-line';
+import { Search } from '@element-plus/icons-vue';
+import { lazyLoadDept, deptMoveUp, deptMoveDown } from '../../api';
+import { warningNotification } from '/@/utils/message';
+import { TreeItemType, APIResponseData } from '../../types';
+import type Node from 'element-plus/es/components/tree/src/model/node';
+</script>
+<style lang="scss">
+</style>
