d2c7ed3b795739b527038de0a70f44f9ef92ed44
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-06 22:54:19 +0800

    Scaffold MenuTreeCom component with type signatures and docstrings

---
+++ cu-cockpit-web/src/views/system/menu/components/MenuTreeCom/index.vue
@@ -0,0 +1,17 @@
+<template>
+
+	<el-input v-model="filterVal" :prefix-icon="Search" placeholder="请输入菜单名称" />
+</template>
+<script lang="ts" setup>
+import { ref, toRaw, watch, h } from 'vue';
+import { ElTree } from 'element-plus';
+import { getElementLabelLine } from 'element-tree-line';
+import { Search } from '@element-plus/icons-vue';
+import SvgIcon from '/@/components/svgIcon/index.vue';
+import { lazyLoadMenu, menuMoveUp, menuMoveDown } from '../../api';
+import { warningNotification } from '/@/utils/message';
+import { TreeTypes, MenuTreeItemType } from '../../types';
+import type Node from 'element-plus/es/components/tree/src/model/node';
+</script>
+<style lang="scss">
+</style>
