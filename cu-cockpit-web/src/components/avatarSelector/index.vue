e0c3e4af09fe4251a4dafec12fd2d231645bed0e
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-06 12:40:19 +0800

    Build index template (index 2)

---
+++ cu-cockpit-web/src/components/avatarSelector/index.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+<script setup>
+import 'vue-cropper/dist/index.css';
+import { VueCropper } from 'vue-cropper';
+import { useUserInfo } from '/@/stores/userInfo';
+import { getCurrentInstance, nextTick, reactive, ref, computed, onMounted, defineExpose } from 'vue';
+import { base64ToFile } from '/@/utils/tools';
+import headerImage from "/@/assets/img/headerImage.png";
+import {getBaseURL} from "/@/utils/baseUrl";
+</script>
+<style lang="scss" scoped>
+</style>
