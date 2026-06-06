449a7df8e94e03b9d9375fc039533db95a2f4cc6
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-06 10:57:29 +0800

    Scaffold App component skeleton with placeholder signatures

---
+++ cu-cockpit-web/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+	<el-config-provider :size="getGlobalComponentSize" :locale="getGlobalI18n">
+		<!-- v-show="themeConfig.lockScreenTime > 1" -->
+		<router-view v-show="themeConfig.lockScreenTime > 1" />
+		<LockScreen v-if="themeConfig.isLockScreen" />
+		<Setings ref="setingsRef" v-show="themeConfig.lockScreenTime > 1" />
+		<CloseFull v-if="!themeConfig.isLockScreen" />
+		<Permissions ref="permissionsRef" />
+<!--		<Upgrade v-if="getVersion" />-->
+	</el-config-provider>
+</template>
+<script setup lang="ts" name="app">
+import { defineAsyncComponent, computed, ref, onBeforeMount, onMounted, onUnmounted, nextTick, watch, onBeforeUnmount } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useI18n } from 'vue-i18n';
+import { storeToRefs } from 'pinia';
+import { useTagsViewRoutes } from '/@/stores/tagsViewRoutes';
+import { useThemeConfig } from '/@/stores/themeConfig';
+import other from '/@/utils/other';
+import { Local, Session } from '/@/utils/storage';
+import mittBus from '/@/utils/mitt';
+import setIntroduction from '/@/utils/setIconfont';
+import { ElMessageBox, ElNotification, NotificationHandle } from 'element-plus';
+import { useCore } from '/@/utils/cores';
+</script>
