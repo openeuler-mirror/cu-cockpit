2e883f71624128913752b9c9030adf1c1065d3de
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-08 19:52:07 +0800

    Wire index script behavior (index)

--- cu-cockpit-web/src/components/table/index.vue
+++ cu-cockpit-web/src/components/table/index.vue
@@ -25,6 +25,7 @@
 					
 </template>
 <script setup lang="ts" name="netxTable">
+
 import { reactive, computed, nextTick, ref } from 'vue';
 import { ElMessage } from 'element-plus';
 import table2excel from 'js-table2excel';
@@ -32,6 +33,124 @@ import Sortable from 'sortablejs';
 import { storeToRefs } from 'pinia';
 import { useThemeConfig } from '/@/stores/themeConfig';
 import '/@/theme/tableTool.scss';
+
+// 定义父组件传过来的值
+const props = defineProps({
+	// 列表内容
+	data: {
+		type: Array<EmptyObjectType>,
+		default: () => [],
+	},
+	// 表头内容
+	header: {
+		type: Array<EmptyObjectType>,
+		default: () => [],
+	},
+	// 配置项
+	config: {
+		type: Object,
+		default: () => {},
+	},
+});
+
+// 定义子组件向父组件传值/事件
+const emit = defineEmits(['delRow', 'pageChange', 'sortHeader']);
+
+// 定义变量内容
+const toolSetRef = ref();
+const storesThemeConfig = useThemeConfig();
+const { themeConfig } = storeToRefs(storesThemeConfig);
+const state = reactive({
+	page: {
+		pageNum: 1,
+		pageSize: 10,
+	},
+	selectlist: [] as EmptyObjectType[],
+	checkListAll: true,
+	checkListIndeterminate: false,
+});
+
+// 设置边框显示/隐藏
+const setBorder = computed(() => {
+	return props.config.isBorder ? true : false;
+});
+// 获取父组件 配置项（必传）
+const getConfig = computed(() => {
+	return props.config;
+});
+// 设置 tool header 数据
+const setHeader = computed(() => {
+	return props.header.filter((v) => v.isCheck);
+});
+// tool 列显示全选改变时
+const onCheckAllChange = <T>(val: T) => {
+	if (val) props.header.forEach((v) => (v.isCheck = true));
+	else props.header.forEach((v) => (v.isCheck = false));
+	state.checkListIndeterminate = false;
+};
+// tool 列显示当前项改变时
+const onCheckChange = () => {
+	const headers = props.header.filter((v) => v.isCheck).length;
+	state.checkListAll = headers === props.header.length;
+	state.checkListIndeterminate = headers > 0 && headers < props.header.length;
+};
+// 表格多选改变时，用于导出
+const onSelectionChange = (val: EmptyObjectType[]) => {
+	state.selectlist = val;
+};
+// 删除当前项
+const onDelRow = (row: EmptyObjectType) => {
+	emit('delRow', row);
+};
+// 分页改变
+const onHandleSizeChange = (val: number) => {
+	state.page.pageSize = val;
+	emit('pageChange', state.page);
+};
+// 分页改变
+const onHandleCurrentChange = (val: number) => {
+	state.page.pageNum = val;
+	emit('pageChange', state.page);
+};
+// 搜索时，分页还原成默认
+const pageReset = () => {
+	state.page.pageNum = 1;
+	state.page.pageSize = 10;
+	emit('pageChange', state.page);
+};
+// 导出
+const onImportTable = () => {
+	if (state.selectlist.length <= 0) return ElMessage.warning('请先选择要导出的数据');
+	table2excel(props.header, state.selectlist, `${themeConfig.value.globalTitle} ${new Date().toLocaleString()}`);
+};
+// 刷新
+const onRefreshTable = () => {
+	emit('pageChange', state.page);
+};
+// 设置
+const onSetTable = () => {
+	nextTick(() => {
+		const sortable = Sortable.create(toolSetRef.value, {
+			handle: '.handle',
+			dataIdAttr: 'data-key',
+			animation: 150,
+			onEnd: () => {
+				const headerList: EmptyObjectType[] = [];
+				sortable.toArray().forEach((val) => {
+					props.header.forEach((v) => {
+						if (v.key === val) headerList.push({ ...v });
+					});
+				});
+				emit('sortHeader', headerList);
+			},
+		});
+	});
+};
+
+// 暴露变量
+defineExpose({
+	pageReset,
+});
 </script>
 <style scoped lang="scss">
 </style>
