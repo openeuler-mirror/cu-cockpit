9693bb464c0bfe265a588d642739f5849b291945
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-08 20:05:17 +0800

    Add styling to the logo component

--- cu-cockpit-web/src/layout/logo/index.vue
+++ cu-cockpit-web/src/layout/logo/index.vue
@@ -46,4 +46,53 @@ const siteLogo = computed(() => {
 
 </script>
 <style scoped lang="scss">
+
+.layout-logo {
+	width: 220px;
+	height: 50px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	box-shadow: rgb(0 21 41 / 2%) 0px 1px 4px;
+	// color: #eaeaea;
+	 color: #333333;
+	font-size: 18px;
+	cursor: pointer;
+	animation: logoAnimation 0.3s ease-in-out;
+
+	span {
+		white-space: nowrap;
+		display: inline-block;
+	}
+
+	&:hover {
+		span {
+			color: var(--color-primary-light-2);
+		}
+	}
+
+	&-medium-img {
+		width: 34px;
+		margin-right: 5px;
+	}
+}
+
+.layout-logo-size {
+	width: 100%;
+	height: 50px;
+	display: flex;
+	cursor: pointer;
+	animation: logoAnimation 0.3s ease-in-out;
+
+	&-img {
+		width: 40px;
+		margin: auto;
+	}
+
+	&:hover {
+		img {
+			animation: logoAnimation 0.3s ease-in-out;
+		}
+	}
+}
 </style>
