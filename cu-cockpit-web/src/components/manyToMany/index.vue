a169fc64597e61dcca518cdf071e0bbef260a498
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-08 15:04:43 +0800

    Wire up index script behavior (index 2)

--- cu-cockpit-web/src/components/manyToMany/index.vue
+++ cu-cockpit-web/src/components/manyToMany/index.vue
@@ -6,7 +6,37 @@
   </div>
 </template>
 <script lang="ts" setup>
+
 import {watch, ref} from "vue";
+
+const props = defineProps({
+  modelValue: Array,
+  bindValue: Array,
+  displayLabel: {
+    type:String,
+    default: ""
+  }
+})
+
+// template上使用data
+const data = ref()
+watch(() => {
+      return props.bindValue
+    }, // 监听modelValue的变化，
+    (value) => {
+      const {displayLabel} = props
+      const result = value ? value.map((item: any) => {
+        return item[displayLabel]
+      }) : null
+      data.value = result
+    }, // 当modelValue值触发后，同步修改data.value的值
+    {immediate: true} // 立即触发一次，给data赋值初始值
+)
+
+const tagType = ['success', 'info', 'warning', 'danger']
+const randomType = (): string => {
+  return tagType[Math.floor(Math.random() * tagType.length)];
+}
 </script>
 <style scoped>
 </style>
