4ade8c56b2fdf1839a1a8589a2bc3b74b4b5e55f
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-08 13:03:16 +0800

    Extend run MemoryItem

--- cu-cockpit-web/src/api/run/run.ts
+++ cu-cockpit-web/src/api/run/run.ts
@@ -6,3 +6,34 @@ type hardMode = 'cpu' | 'disk' | 'network' | 'system' | 'bios' | 'os_system' | '
  * @param mode 
  * @returns 
  */
+export const monitorStatus = (mode: monitorMode) => {
+    return request({
+        url: '/rescrouce/monitor/monitor_status.sh',
+        method: 'get',
+        params: { mode },
+    });
+}
+
+
+/**
+ * 获取硬件静态信息
+ * @param mode 
+ * @returns 
+ */
+export const hardInfo = (mode: hardMode) => {
+    return request({
+        url: '/rescrouce/monitor/hard_info.sh',
+        method: 'get',
+        params: { mode },
+    });
+}
+
+interface MemoryItem {
+  ID: string;
+  内存拓扑: string;
+  类型: string;
+  大小: string;
+  状态: string;
+  Rank: string;
+  速度: string;
+}
