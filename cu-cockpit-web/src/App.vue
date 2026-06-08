f664e6847d671fdeb8bf407a1be5ef73b2f2519f
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-08 23:45:15 +0800

    Wire up reactive state and connect event handlers

--- cu-cockpit-web/src/App.vue
+++ cu-cockpit-web/src/App.vue
@@ -11,6 +11,7 @@
 	</el-config-provider>
 </template>
 <script setup lang="ts" name="app">
+
 import { defineAsyncComponent, computed, ref, onBeforeMount, onMounted, onUnmounted, nextTick, watch, onBeforeUnmount } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { useI18n } from 'vue-i18n';
@@ -21,6 +22,37 @@ import other from '/@/utils/other';
 import { Local, Session } from '/@/utils/storage';
 import mittBus from '/@/utils/mitt';
 import setIntroduction from '/@/utils/setIconfont';
+
+// 引入组件
+const LockScreen = defineAsyncComponent(() => import('/@/layout/lockScreen/index.vue'));
+const Setings = defineAsyncComponent(() => import('/@/layout/navBars/breadcrumb/setings.vue'));
+const CloseFull = defineAsyncComponent(() => import('/@/layout/navBars/breadcrumb/closeFull.vue'));
+const Upgrade = defineAsyncComponent(() => import('/@/layout/upgrade/index.vue'));
+const Permissions = defineAsyncComponent(() => import('/@/layout/navBars/breadcrumb/permissions.vue'));
 import { ElMessageBox, ElNotification, NotificationHandle } from 'element-plus';
 import { useCore } from '/@/utils/cores';
+// 定义变量内容
+const { messages, locale } = useI18n();
+const setingsRef = ref();
+const permissionsRef= ref();
+const route = useRoute();
+const stores = useTagsViewRoutes();
+const storesThemeConfig = useThemeConfig();
+const { themeConfig } = storeToRefs(storesThemeConfig);
+const core = useCore();
+const router = useRouter();
+// 获取版本号
+const getVersion = computed(() => {
+	let isVersion = false;
+	if (route.path !== '/login') {
+		// @ts-ignore
+		if ((Local.get('version') && Local.get('version') !== __VERSION__) || !Local.get('version')) isVersion = true;
+	}
+	return isVersion;
+});
+// 获取全局组件大小
+const getGlobalComponentSize = computed(() => {
+	return other.globalComponentSize();
+});
+// 获取全局 i18n
 </script>
