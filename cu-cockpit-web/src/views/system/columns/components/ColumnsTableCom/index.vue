c44a6853e6fda7f11a06fabd2566a523dfdd9166
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-09 15:06:41 +0800

    Build out template structure for columns table

--- cu-cockpit-web/src/views/system/columns/components/ColumnsTableCom/index.vue
+++ cu-cockpit-web/src/views/system/columns/components/ColumnsTableCom/index.vue
@@ -2,12 +2,121 @@
 
 </template>
 <script lang="ts" setup>
+
 import { ref, reactive } from 'vue';
 import { ElMessageBox } from 'element-plus';
 import ColumnsFormCom from '../ColumnsFormCom/index.vue';
 import { getColumnsData, automatchColumnsData, deleteColumnsData, updateColumnsData } from './api';
 import { successNotification, warningNotification } from '/@/utils/message';
 import { APIResponseData, CurrentInfoType, ColumnsFormDataType, AddColumnsDataType } from '../../types';
+
+const props = defineProps({
+	currentInfo: {
+		type: Object as () => CurrentInfoType,
+		required: true,
+		default: () => {},
+	},
+});
+
+let searchParams = reactive({
+	page: 1,
+	limit: 20,
+});
+let state = reactive({
+	loading: false,
+	data: [],
+	total: 0,
+});
+let drawerVisible = ref(false);
+let drawerFormData = ref<Partial<ColumnsFormDataType>>({});
+
+const fetchData = async (query: CurrentInfoType = props.currentInfo) => {
+	try {
+		state.loading = true;
+		const res = await getColumnsData({ ...searchParams, ...query });
+		if (res?.code === 2000) {
+			state.data = res.data;
+			state.total = res.total;
+		}
+	} finally {
+		state.loading = false;
+	}
+};
+
+/**
+ * 自动匹配列
+ */
+const handleAutomatch = async () => {
+	if (props.currentInfo?.role && props.currentInfo?.model && props.currentInfo?.app) {
+		const res = await automatchColumnsData(props.currentInfo);
+		if (res?.code === 2000) {
+			successNotification('匹配成功');
+			fetchData();
+		}
+		return;
+	}
+	warningNotification('请选择角色和模型表！');
+};
+
+/**
+ * 新增 or 编辑
+ */
+const handleUpdateColumn = (type: string, record?: ColumnsFormDataType) => {
+	if (props.currentInfo?.role && props.currentInfo?.model && props.currentInfo?.app) {
+		if (type === 'update' && record) {
+			drawerFormData.value = record;
+		}
+		drawerVisible.value = true;
+		return;
+	}
+	warningNotification('请选择角色和模型表！');
+};
+const handleDrawerClose = (type?: string) => {
+	if (type === 'submit') {
+		fetchData();
+	}
+	drawerVisible.value = false;
+	drawerFormData.value = {};
+};
+
+/**
+ * 删除 deleteColumnsData
+ */
+const handleDelete = ({ id }: { id: number }) => {
+	ElMessageBox.confirm('确定删除该字段吗？', '提示', {
+		type: 'error',
+		confirmButtonText: '确定',
+		cancelButtonText: '取消',
+	})
+		.then(async () => {
+			const res = await deleteColumnsData(id);
+			if (res?.code === 2000) {
+				successNotification('删除成功');
+				fetchData();
+			}
+		})
+		.catch(() => {});
+};
+
+const handleChange = (record: AddColumnsDataType) => {
+	updateColumnsData(record).then((res: APIResponseData) => {
+		successNotification(res.msg || '更新成功');
+	});
+};
+
+/**
+ * 分页
+ */
+const handleSizeChange = (limit: number) => {
+	searchParams.limit = limit;
+	fetchData();
+};
+const handleCurrentChange = (page: number) => {
+	searchParams.page = page;
+	fetchData();
+};
+
+defineExpose({ fetchData });
 </script>
 <style lang="scss" scoped>
 </style>
