4fe44b8221e9fc0f9da4fd4a7ebd288a1ddc747d
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-09 14:32:19 +0800

    Plug in script behavior, adding ~70 lines

--- cu-cockpit-web/src/views/system/columns/components/ColumnsFormCom/index.vue
+++ cu-cockpit-web/src/views/system/columns/components/ColumnsFormCom/index.vue
@@ -30,11 +30,81 @@
 	</div>
 </template>
 <script lang="ts" setup>
+
 import { reactive, ref, onMounted } from 'vue';
 import { addColumnsData, updateColumnsData } from '../ColumnsTableCom/api';
 import { successNotification } from '/@/utils/message';
 import { CurrentInfoType, ColumnsFormDataType } from '../../types';
 import type { FormInstance } from 'element-plus';
+
+const props = defineProps({
+	currentInfo: {
+		type: Object as () => CurrentInfoType,
+		required: true,
+		default: () => {},
+	},
+	initFormData: {
+		type: Object as () => Partial<ColumnsFormDataType>,
+		default: () => {},
+	},
+});
+const emit = defineEmits(['drawerClose']);
+
+const formRef = ref<FormInstance>();
+const formRules = reactive({
+	field_name: [{ required: true, message: '请输入字段名！', trigger: 'blur' }],
+	title: [{ required: true, message: '请输入列名！', trigger: 'blur' }],
+});
+
+let formData = reactive<ColumnsFormDataType>({
+	field_name: '',
+	title: '',
+	is_create: true,
+	is_update: true,
+	is_query: true,
+});
+let btnLoading = ref(false);
+
+const setMenuFormData = () => {
+	if (props.initFormData?.id) {
+		formData.id = props.initFormData?.id || '';
+		formData.field_name = props.initFormData.field_name || '';
+		formData.title = props.initFormData.title || '';
+		formData.is_create = !!props.initFormData.is_create;
+		formData.is_update = !!props.initFormData.is_update;
+		formData.is_query = !!props.initFormData.is_query;
+	}
+};
+
+const handleSubmit = () => {
+	formRef.value?.validate(async (valid) => {
+		if (!valid) return;
+		try {
+			btnLoading.value = true;
+			let res;
+			if (formData.id) {
+				res = await updateColumnsData({ ...formData, ...props.currentInfo });
+			} else {
+				res = await addColumnsData({ ...formData, ...props.currentInfo });
+			}
+			if (res?.code === 2000) {
+				successNotification(res.msg as string);
+				handleClose('submit');
+			}
+		} finally {
+			btnLoading.value = false;
+		}
+	});
+};
+
+const handleClose = (type: string = '') => {
+	emit('drawerClose', type);
+	formRef.value?.resetFields();
+};
+
+onMounted(() => {
+	setMenuFormData();
+});
 </script>
 <style lang="scss" scoped>
 </style>
