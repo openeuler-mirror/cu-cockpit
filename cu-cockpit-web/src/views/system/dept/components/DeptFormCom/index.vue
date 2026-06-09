f9cf13553ef22235300520bd30896c143d1ab33e
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-09 16:34:43 +0800

    Wire up index script behavior (index 3)

--- cu-cockpit-web/src/views/system/dept/components/DeptFormCom/index.vue
+++ cu-cockpit-web/src/views/system/dept/components/DeptFormCom/index.vue
@@ -34,12 +34,107 @@
 	</el-form>
 </template>
 <script lang="ts" setup>
+
 import { reactive, ref, onMounted } from 'vue';
 import { ElForm, FormRules } from 'element-plus';
 import { lazyLoadDept, AddObj, UpdateObj } from '../../api';
 import { successNotification } from '/@/utils/message';
 import { DeptFormDataType, TreeItemType, APIResponseData } from '../../types';
 import type Node from 'element-plus/es/components/tree/src/model/node';
+
+interface IProps {
+	initFormData: TreeItemType | null;
+	treeData: TreeItemType[];
+	cacheData: TreeItemType[];
+}
+
+const defaultTreeProps: any = {
+	children: 'children',
+	label: 'name',
+	value: 'id',
+	isLeaf: (data: TreeItemType[], node: Node) => {
+		if (node?.data.hasChild) {
+			return false;
+		} else {
+			return true;
+		}
+	},
+};
+
+const formRef = ref<InstanceType<typeof ElForm>>();
+const rules = reactive<FormRules>({
+	name: [{ required: true, message: '部门名称必填', trigger: 'blur' }],
+	key: [{ required: true, message: '部门标识必填', trigger: 'blur' }],
+});
+
+const props = withDefaults(defineProps<IProps>(), {
+	initFormData: () => null,
+	treeData: () => [],
+	cacheData: () => [],
+});
+const emit = defineEmits(['drawerClose']);
+
+let deptDefaultList = ref<TreeItemType[]>([]);
+let deptFormData = reactive<DeptFormDataType>({
+	key: '',
+	parent: '',
+	name: '',
+	owner: '',
+	description: '',
+});
+let deptBtnLoading = ref(false);
+
+const setDeptFormData = () => {
+	if (props.initFormData?.id) {
+		deptFormData.id = props.initFormData?.id;
+		deptFormData.key = props.initFormData.key || '';
+		deptFormData.parent = props.initFormData.parent || '';
+		deptFormData.name = props.initFormData.name || '';
+		deptFormData.owner = props.initFormData.owner || '';
+		deptFormData.description = props.initFormData.description || '';
+	}
+};
+
+const handleTreeLoad = (node: Node, resolve: Function) => {
+	if (node.level !== 0) {
+		lazyLoadDept({ parent: node.data.id }).then((res: APIResponseData) => {
+			resolve(res.data);
+		});
+	}
+};
+
+const handleUpdateMenu = () => {
+	formRef.value?.validate(async (valid) => {
+		if (!valid) return;
+		try {
+			let res;
+			deptBtnLoading.value = true;
+			if (deptFormData.id) {
+				res = await UpdateObj(deptFormData);
+			} else {
+				res = await AddObj(deptFormData);
+			}
+			if (res?.code === 2000) {
+				successNotification(res.msg as string);
+				handleClose('submit');
+			}
+		} finally {
+			deptBtnLoading.value = false;
+		}
+	});
+};
+
+const handleClose = (type: string = '') => {
+	emit('drawerClose', type);
+	formRef.value?.resetFields();
+};
+
+onMounted(async () => {
+	props.treeData.map((item) => {
+		deptDefaultList.value.push(item);
+	});
+	setDeptFormData();
+});
 </script>
 <style lang="scss" scoped>
 </style>
