a9985bf2b54e3a677e68ff0c6b8b281ab95383c0
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-09 16:04:43 +0800

    Hook up basic script wiring to crudTable

--- cu-cockpit-web/src/views/system/config/components/components/crudTable.vue
+++ cu-cockpit-web/src/views/system/config/components/components/crudTable.vue
@@ -6,6 +6,13 @@
    </div>
 </template>
 <script setup lang="ts">
+
 import {computed, defineComponent, onMounted, watch} from "vue";
 import {CreateCrudOptionsProps, CreateCrudOptionsRet, useFs, AddReq,
+  compute,
+  DelReq,
+  dict,
+  EditReq,
+  UserPageQuery,
+  UserPageRes} from "@fast-crud/fast-crud";
 </script>
