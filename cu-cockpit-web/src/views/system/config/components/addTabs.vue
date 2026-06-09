815a83d8a9c424205db68b5d28bba622edcfb486
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-09 15:48:26 +0800

    Hook up addTabs interactive logic

--- cu-cockpit-web/src/views/system/config/components/addTabs.vue
+++ cu-cockpit-web/src/views/system/config/components/addTabs.vue
@@ -16,10 +16,54 @@
 	</div>
 </template>
 <script setup lang="ts">
+
 import * as api from '../api';
 import {ref, reactive, inject} from 'vue';
 import type { FormInstance, FormRules } from 'element-plus';
 import { successMessage } from '/@/utils/message';
+
+let form = reactive({
+	title: null,
+	key: null,
+});
+const formRef = ref<FormInstance>();
+const rules = reactive<FormRules>({
+	title: [
+		{
+			required: true,
+			message: '请输入',
+		},
+	],
+	key: [
+		{
+			required: true,
+			message: '请输入',
+		},
+		{
+			pattern: /^[A-Za-z0-9]+$/,
+			message: '只能是英文和数字',
+		},
+	],
+});
+
+
+const refreshView:any = inject('refreshView')
+const onSubmit = async (formEl: FormInstance | undefined) => {
+	if (!formEl) return;
+	await formEl.validate((valid, fields) => {
+		if (valid) {
+			api.AddObj(form).then((res: any) => {
+				if (res.code == 2000) {
+          successMessage('新增成功');
+          refreshView()
+        }
+
+			});
+		} else {
+			console.log('error submit!', fields);
+		}
+	});
+};
 </script>
 <style>
 </style>
