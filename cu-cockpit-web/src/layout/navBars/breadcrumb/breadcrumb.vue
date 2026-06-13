33a2bdfd0c16cf4b4a045dca277399fe5908471b
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-13 15:43:57 +0800

    Complete breadcrumb component

--- cu-cockpit-web/src/layout/navBars/breadcrumb/breadcrumb.vue
+++ cu-cockpit-web/src/layout/navBars/breadcrumb/breadcrumb.vue
@@ -1,5 +1,4 @@
 <template>
-
 	<div v-if="isShowBreadcrumb" class="layout-navbars-breadcrumb">
 		<SvgIcon
 			class="layout-navbars-breadcrumb-icon"
@@ -23,8 +22,8 @@
 		</el-breadcrumb>
 	</div>
 </template>
-<script setup lang="ts" name="layoutBreadcrumb">
 
+<script setup lang="ts" name="layoutBreadcrumb">
 import { reactive, computed, onMounted } from 'vue';
 import { onBeforeRouteUpdate, useRoute, useRouter } from 'vue-router';
 import { Local } from '/@/utils/storage';
@@ -105,5 +104,43 @@ onBeforeRouteUpdate((to) => {
 	initRouteSplit(to.path);
 });
 </script>
+
 <style scoped lang="scss">
+.layout-navbars-breadcrumb {
+	flex: 1;
+	height: inherit;
+	display: flex;
+	align-items: center;
+	.layout-navbars-breadcrumb-icon {
+		cursor: pointer;
+		font-size: 18px;
+		color: var(--next-bg-topBarColor);
+		height: 100%;
+		width: 40px;
+		opacity: 0.8;
+		&:hover {
+			opacity: 1;
+		}
+	}
+	.layout-navbars-breadcrumb-span {
+		display: flex;
+		opacity: 0.7;
+		color: var(--next-bg-topBarColor);
+	}
+	.layout-navbars-breadcrumb-iconfont {
+		font-size: 14px;
+		margin-right: 5px;
+	}
+	:deep(.el-breadcrumb__separator) {
+		opacity: 0.7;
+		color: var(--next-bg-topBarColor);
+	}
+	:deep(.el-breadcrumb__inner a, .el-breadcrumb__inner.is-link) {
+		font-weight: unset !important;
+		color: var(--next-bg-topBarColor);
+		&:hover {
+			color: var(--el-color-primary) !important;
+		}
+	}
+}
 </style>
