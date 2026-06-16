fead377410702f4c015a7568019c70e0ba9d6ecf
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-16 18:36:54 +0800

    Wire crud options

--- cu-cockpit-web/src/views/system/menu/components/MenuButtonCom/crud.tsx
+++ cu-cockpit-web/src/views/system/menu/components/MenuButtonCom/crud.tsx
@@ -22,8 +22,24 @@ export const createCrudOptions = function ({crudExpose, context}: CreateCrudOpti
     const delRequest = async ({row}: DelReq) => {
         return await api.DelObj(row.id);
     };
+    const addRequest = async ({form}: AddReq) => {
+        return await api.AddObj({...form, ...{menu: context!.selectOptions.value.id}});
+    };
     return {
         crudOptions: {
+            pagination:{
+                show:false
+            },
+            search: {
+                container: {
+                    action: {
+                        //按钮栏配置
+                        col: {
+                            span: 8,
+                        },
+                    },
+                },
+            },
         },
     };
 };
