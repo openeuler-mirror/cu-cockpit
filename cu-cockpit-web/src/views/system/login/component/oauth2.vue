f196ef4eec04bf02aefc88b3f9f0b8298a8272b9
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-23 03:46:18 +0800

    Build oauth2 template

---
+++ cu-cockpit-web/src/views/system/login/component/oauth2.vue
@@ -0,0 +1,19 @@
+<template>
+
+	<div class="other-fast-way" v-if="backends.length">
+		<div class="fast-title"><span>其他快速方式登录</span></div>
+		<ul class="fast-list">
+			<li v-for="(v, k) in backends" :key="v">
+				<a @click.once="handleOAuth2LoginClick(v)" style="width: 50px;color: #18bc9c">
+					<img :src="v.icon" :alt="v.app_name" />
+										<p>{{ v.app_name }}</p>
+
+				</a>
+			</li>
+		</ul>
+	</div>
+</template>
+<script lang="ts">
+</script>
+<style scoped lang="scss">
+</style>
