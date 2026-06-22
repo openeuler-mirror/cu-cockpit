ca837ebd8ba97432f9acb98f4c64eaf550ef0820
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-22 18:12:07 +0800

    Scaffold detailPage template structure

---
+++ cu-cockpit-web/src/views/storage/detailPage.vue
@@ -0,0 +1,14 @@
+<template #header>
+
+                    <div class="card-header">
+                        <span>{{ device.pttype ? device.pttype.toUpperCase() + ' 分区' : '分区' }}</span>
+                    </div>
+                
+</template>
+<script lang="ts" setup name="storageDetail">
+import { onMounted, ref } from 'vue';
+import { hardInfo } from '/@/api/run/run';
+import { useRouter } from 'vue-router';
+</script>
+<style scoped lang="scss">
+</style>
