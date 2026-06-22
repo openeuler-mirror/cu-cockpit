d1ad73f7497066d2320a300e18ea20c0af4908bb
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-22 18:59:51 +0800

    Scaffold CRUD component with type signatures and docstrings

---
+++ cu-cockpit-web/src/views/system/areas/crud.tsx
@@ -0,0 +1,18 @@
+import * as api from './api';
+import { dict, UserPageQuery, AddReq, DelReq, EditReq, compute, CreateCrudOptionsProps, CreateCrudOptionsRet } from '@fast-crud/fast-crud';
+import { dictionary } from '/@/utils/dictionary';
+import { successMessage } from '/@/utils/message';
+import { auth } from '/@/utils/authFunction';
+import tableSelector from '/@/components/tableSelector/index.vue';
+import { shallowRef } from 'vue';
+
+
+export const createCrudOptions = function ({ crudExpose }: CreateCrudOptionsProps): CreateCrudOptionsRet {
+	const pageRequest = async (query: UserPageQuery) => {
+		return await api.GetList(query);
+	};
+    return {
+        crudOptions: {
+        },
+    };
+};
