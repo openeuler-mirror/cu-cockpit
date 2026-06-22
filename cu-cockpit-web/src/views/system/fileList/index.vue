b8f2f96bdb2cac4bac5464d1371fc765528bb036
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-23 00:33:53 +0800

    Scaffold index template

---
+++ cu-cockpit-web/src/views/system/fileList/index.vue
@@ -0,0 +1,34 @@
+<template #cell_preview="scope">
+
+        <div v-if="scope.row.file_type === 0">
+          <el-image style="width: 100%; aspect-ratio: 1 /1 ;" :src="getBaseURL(scope.row.url)"
+            :preview-src-list="[getBaseURL(scope.row.url)]" :preview-teleported="true" />
+        </div>
+        <div v-if="scope.row.file_type === 1" class="_preview"
+          @click="openPreviewHandle(getBaseURL(scope.row.url), 'video')">
+          <el-icon :size="60">
+            <VideoCamera />
+          </el-icon>
+        </div>
+        <div v-if="scope.row.file_type === 2" class="_preview"
+          @click="openPreviewHandle(getBaseURL(scope.row.url), 'video')">
+          <el-icon :size="60">
+            <Headset />
+          </el-icon>
+        </div>
+        <el-icon v-if="scope.row.file_type === 3" :size="60">
+          <Document />
+        </el-icon>
+        <div v-if="scope.row.file_type > 3">未知类型</div>
+      
+</template>
+<script lang="ts" setup>
+import { ref, onMounted, nextTick } from 'vue';
+import { useExpose, useCrud } from '@fast-crud/fast-crud';
+import { createCrudOptions } from './crud';
+import { getBaseURL } from '/@/utils/baseUrl';
+import FileSelector from '/@/components/fileSelector/index.vue';
+import { SHOW } from '/@/components/fileSelector/types';
+</script>
+<style lang="css" scoped>
+</style>
