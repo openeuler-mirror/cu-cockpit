e76d8f83ed91273d4c3a73fd4ce6a3ec1c65a25a
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-23 01:10:57 +0800

    Scaffold login log CRUD with empty function stubs

---
+++ cu-cockpit-web/src/views/system/log/loginLog/crud.tsx
@@ -0,0 +1,14 @@
+import * as api from './api';
+import { UserPageQuery, AddReq, DelReq, EditReq, CreateCrudOptionsProps, CreateCrudOptionsRet, dict } from '@fast-crud/fast-crud';
+import {commonCrudConfig} from "/@/utils/commonCrud";
+
+
+export const createCrudOptions = function ({ crudExpose }: CreateCrudOptionsProps): CreateCrudOptionsRet {
+	const pageRequest = async (query: UserPageQuery) => {
+		return await api.GetList(query);
+	};
+    return {
+        crudOptions: {
+        },
+    };
+};
