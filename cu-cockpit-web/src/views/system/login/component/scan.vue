03e8a5484a476ea4fe0cf06a54d9bebc6ad378e0
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-23 03:07:01 +0800

    Wire up scan script skeleton with signatures and docstrings

---
+++ cu-cockpit-web/src/views/system/login/component/scan.vue
@@ -0,0 +1,23 @@
+<template>
+
+	<div class="login-scan-container">
+		<div ref="qrcodeRef"></div>
+		<div class="font12 mt20 login-msg">{{ $t('message.scan.text') }}</div>
+	</div>
+</template>
+<script lang="ts">
+
+import { ref, defineComponent, onMounted } from 'vue';
+import QRCode from 'qrcodejs2-fixes';
+
+export default defineComponent({
+	name: 'loginScan',
+	setup() {
+		const qrcodeRef = ref<HTMLElement | null>(null);
+		
+		return { qrcodeRef };
+	},
+});
+</script>
+<style scoped lang="scss">
+</style>
