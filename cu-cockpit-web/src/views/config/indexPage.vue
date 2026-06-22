57aabda7c94311a03d91a14f65d84e4606e52f10
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-22 16:04:02 +0800

    Scaffold indexPage Vue component template

---
+++ cu-cockpit-web/src/views/config/indexPage.vue
@@ -0,0 +1,16 @@
+<template #label>
+
+                                <div class="cell-item">SHA256</div>
+                            
+</template>
+<script lang="ts" setup name="configIndex">
+import { ref, reactive, onMounted, onUnmounted, computed } from 'vue';
+import { configGet, hostSet, timeSet, configUpdate } from '/@/api/config/config';
+import type { FormInstance, FormRules } from 'element-plus';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import dayjs from 'dayjs'
+import { storeToRefs } from 'pinia';
+import { userPermissiom } from '/@/stores/userPermissiom';
+</script>
+<style scoped lang="scss">
+</style>
