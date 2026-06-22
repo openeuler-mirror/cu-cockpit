c6db8afe0166d5a5b50c69fac3b3aaf6f1820969
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-22 23:17:09 +0800

    Skeleton CRUD component with type signatures and docstrings

---
+++ cu-cockpit-web/src/views/system/downloadCenter/crud.tsx
@@ -0,0 +1,17 @@
+import { CrudOptions, AddReq, DelReq, EditReq, dict, CrudExpose, compute } from '@fast-crud/fast-crud';
+import * as api from './api';
+import { dictionary } from '/@/utils/dictionary';
+import { successMessage } from '../../../utils/message';
+import { auth } from '/@/utils/authFunction';
+import { getBaseURL } from '/@/utils/baseUrl';
+
+
+export const createCrudOptions = function ({ crudExpose }: { crudExpose: CrudExpose; }): CreateCrudOptionsTypes {
+    const pageRequest = async (query: any) => {
+        return await api.GetList(query);
+    };
+    return {
+        crudOptions: {
+        },
+    };
+};
