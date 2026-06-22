89836f9b684562d4bb23a7fe7777b062e63dbe70
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-23 02:51:56 +0800

    Build changePwd template

---
+++ cu-cockpit-web/src/views/system/login/component/changePwd.vue
@@ -0,0 +1,12 @@
+<template #suffix>
+
+					<i class="iconfont el-input__icon login-content-password"
+						:class="isShowPassword ? 'icon-yincangmima' : 'icon-xianshimima'"
+						@click="isShowPassword = !isShowPassword">
+					</i>
+				
+</template>
+<script lang="ts">
+</script>
+<style scoped lang="scss">
+</style>
