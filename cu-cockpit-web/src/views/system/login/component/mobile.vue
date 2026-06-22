93f3a220429c0d272738b984d10180181b963367
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-23 02:56:54 +0800

    Sketch mobile component structure with function signatures and docstring

---
+++ cu-cockpit-web/src/views/system/login/component/mobile.vue
@@ -0,0 +1,33 @@
+<template #prefix>
+
+						<el-icon class="el-input__icon"><ele-Position /></el-icon>
+					
+</template>
+<script lang="ts">
+
+import { toRefs, reactive, defineComponent } from 'vue';
+
+// 定义接口来定义对象的类型
+interface LoginMobileState {
+	username: any;
+	code: string | number | undefined;
+}
+
+// 定义对象与类型
+const ruleForm: LoginMobileState = {
+	username: '',
+	code: '',
+};
+
+export default defineComponent({
+	name: 'loginMobile',
+	setup() {
+		const state = reactive({ ruleForm });
+		return {
+			...toRefs(state),
+		};
+	},
+});
+</script>
+<style scoped lang="scss">
+</style>
