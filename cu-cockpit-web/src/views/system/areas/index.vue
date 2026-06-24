30788fe314311b9e0586f261d34dbf2f7b347e0e
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-24 19:26:19 +0800

    Complete getMenuList API implementation

--- cu-cockpit-web/src/views/system/areas/index.vue
+++ cu-cockpit-web/src/views/system/areas/index.vue
@@ -1,11 +1,10 @@
 <template>
-
 	<fs-page>
 		<fs-crud ref="crudRef" v-bind="crudBinding"> </fs-crud>
 	</fs-page>
 </template>
-<script lang="ts" setup name="areas">
 
+<script lang="ts" setup name="areas">
 import { onMounted } from 'vue';
 import { useFs } from '@fast-crud/fast-crud';
 import { createCrudOptions } from './crud';
