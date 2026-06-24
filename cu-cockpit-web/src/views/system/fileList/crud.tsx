145e743779602d9c26fb5c8e7bf2df62c86896f3
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-24 23:27:12 +0800

    Hook up CRUD options, add ~10 lines of binding code

--- cu-cockpit-web/src/views/system/fileList/crud.tsx
+++ cu-cockpit-web/src/views/system/fileList/crud.tsx
@@ -18,8 +18,18 @@ export const createCrudOptions = function ({ crudExpose, context }: CreateCrudOp
   const pageRequest = async (query: UserPageQuery) => {
     return await api.GetList(query);
   };
+  const editRequest = async ({ form, row }: EditReq) => {
+    form.id = row.id;
+    return await api.UpdateObj(form);
+  };
     return {
         crudOptions: {
+            component: {
+              props: {
+                clearable: true,
+              },
+              placeholder: '请输入关键词',
+            },
         },
     };
 };
