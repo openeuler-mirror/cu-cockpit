513a3f1bacc10e4dd10f7216de5153eeb94ee224
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-24 20:04:44 +0800

    Wire up DelObj endpoint for object deletion.

--- cu-cockpit-web/src/views/system/config/api.ts
+++ cu-cockpit-web/src/views/system/config/api.ts
@@ -16,3 +16,27 @@ export function GetObj(id: InfoReq) {
 		method: 'get',
 	});
 }
+
+export function AddObj(obj: AddReq) {
+	return request({
+		url: apiPrefix,
+		method: 'post',
+		data: obj,
+	});
+}
+
+export function UpdateObj(obj: EditReq) {
+	return request({
+		url: apiPrefix + obj.id + '/',
+		method: 'put',
+		data: obj,
+	});
+}
+
+export function DelObj(id: DelReq) {
+	return request({
+		url: apiPrefix + id + '/',
+		method: 'delete',
+		data: { id },
+	});
+}
