6103677f57e6a97270110a3f0cdb6c589aec5c85
yuesl6 <yuesl6@chinaunicom.cn>
2026-06-29 10:45:21 +0800

    Add column rendering and formatting

--- cu-cockpit-web/src/views/system/user/crud.tsx
+++ cu-cockpit-web/src/views/system/user/crud.tsx
@@ -35,6 +35,9 @@ export const createCrudOptions = function ({ crudExpose }: CreateCrudOptionsProp
     const addRequest = async ({ form }: AddReq) => {
         return await api.AddObj(form);
     };
+    const exportRequest = async (query: UserPageQuery) => {
+        return await api.exportData(query)
+    }
     return {
         crudOptions: {
             table: {
@@ -58,6 +61,58 @@ export const createCrudOptions = function ({ crudExpose }: CreateCrudOptionsProp
                         width: '70px',
                         columnSetDisabled: true, //禁止在列设置中选择
                     },
+                },
+                username: {
+                    title: '账号',
+                    search: {
+                        show: true,
+                    },
+                    type: 'input',
+                    column: {
+                        minWidth: 100, //最小列宽
+                    },
+                    form: {
+                        rules: [
+                            // 表单校验规则
+                            {
+                                required: true,
+                                message: '账号必填项',
+                            },
+                        ],
+                        component: {
+                            placeholder: '请输入账号',
+                        },
+                    },
+                },
+                password: {
+                    title: '密码',
+                    type: 'password',
+                    column: {
+                        show: false,
+                    },
+                    editForm: {
+                        show: false,
+                    },
+                    form: {
+                        rules: [
+                            // 表单校验规则
+                            {
+                                required: true,
+                                message: '密码必填项',
+                            },
+                        ],
+                        component: {
+
+                            span: 12,
+                            showPassword: true,
+                            placeholder: '请输入密码',
+                        },
+                    },
+                    valueResolve({ form }) {
+                        if (form.password) {
+                            form.password = Md5.hashStr(form.password)
+                        }
+                    }
                 },
             },
         },
